b6d2e837dba650e68942527ae68bcddb8070ae26
murali <muralinetha38@gmail.com>
2024-10-12 05:13:03 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true, // or '0.0.0.0' to bind to all interfaces
-  },
   plugins: [react()],
-  server:{
-    port:5173
-  }
 })
